84b5d2f4820dddd40e34e060026f1b6e0da6f152
Matt Eva <matteweva@gmail.com>
2025-07-06 18:15:11 -0700

    created header component

---
+++ src/components/+header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<header>
+	<h1>FreeCreate</h1>
+	<nav>
+		<a href="/donate">Donate</a>
+		<a href="/about">About</a>
+		<a href="/app">App</a>
+		<a href="/profile">profile</a>
+	</nav>
+</header>
+
+<style>
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-<slot />
+<script lang="ts">
+	import Header from '../components/+header.svelte';
+	let { children } = $props();
+</script>
+
+<Header />
+
+{@render children()}
