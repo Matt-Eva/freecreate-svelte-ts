edd4c0797825fcc30fac70a2136adbf43e04870b
Matt Eva <matteweva@gmail.com>
2025-07-08 20:47:12 -0700

    started formatting header

--- src/components/+header.svelte
+++ src/components/+header.svelte
@@ -1,15 +1,63 @@
 <script lang="ts">
+	const renderFullscreen = () => {
+		if (window) {
+			if (document.fullscreenElement) return document.exitFullscreen();
+			document.body.requestFullscreen();
+		}
+	};
 </script>
 
 <header>
-	<h1>FreeCreate</h1>
 	<nav>
-		<a href="/donate">Donate</a>
-		<a href="/about">About</a>
-		<a href="/app">App</a>
-		<a href="/profile">profile</a>
+		<h1><a href="/">FreeCreate</a></h1>
+		<a href="/donate" class="link">Donate</a>
+		<a href="/about" class="link">About</a>
+		<a href="/app" class="link">App</a>
+		<button class="fullscreen" on:click={renderFullscreen}>Fullscreen</button>
+		<a href="/profile" class="profile">profile</a>
 	</nav>
 </header>
 
 <style>
+	header {
+		width: 100%;
+		padding: 0 5px;
+	}
+
+	nav {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		align-items: center;
+	}
+
+	h1 {
+		cursor: pointer;
+	}
+
+	.link {
+		margin-top: 5px;
+		margin-left: 10px;
+	}
+
+	.fullscreen {
+		display: none;
+	}
+
+	.profile {
+		margin-top: 5px;
+		margin-left: auto;
+	}
+
+	@media (min-width: 480px) {
+		.fullscreen {
+			display: block;
+			text-decoration: none;
+			background-color: var(--global-background);
+			border: none;
+			padding: 0px;
+			margin-top: 5px;
+			margin-left: 10px;
+		}
+	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+	import '../globals.css';
 	import Header from '../components/+header.svelte';
 	let { children } = $props();
 </script>
 
-<Header />
+<div class="layout">
+	<Header />
+	{@render children()}
+</div>
 
-{@render children()}
+<style>
+	.layout {
+		height: 100%;
+		width: 100%;
+		max-width: 1200px;
+		justify-self: center;
+		display: grid;
+		grid-template-rows: 40px calc(100% - 80px) 40px;
+		grid-template-columns: 100%;
+	}
+</style>
