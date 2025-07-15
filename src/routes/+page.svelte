35421d17a0e00ef73456e19735cb52cf84f64547
Matt Eva <matteweva@gmail.com>
2025-07-14 18:47:51 -0700

    set up successful resend test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { json } from '@sveltejs/kit';
+
+	const { data } = $props();
+	async function sendEmail() {
+		const apiBase = data.apiBase;
+		try {
+			const res = await fetch(apiBase + '/email', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					email: 'matteweva@gmail.com'
+				})
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	}
+</script>
+
+<button onclick={sendEmail}>send email</button>
