6111fac128a9ee09d795c51f799b42470d163cbd
Matt Eva <matteweva@gmail.com>
2025-08-18 14:21:54 -0700

    known typescript bug for sveltekit route params slug

--- src/routes/new-chapter/[writingId]/+page.ts
+++ src/routes/new-chapter/[writingId]/+page.ts
@@ -1,11 +1,8 @@
-import { error } from '@sveltejs/kit';
 import type { PageLoad } from '../../$types';
 
 export const load: PageLoad = ({ params }) => {
-	const { writingId } = params;
-	if (params.writingId) {
-		return {
-			writingId: params.writingId
-		};
-	}
+	console.log(params);
+	return {
+		writingId: params.writingId
+	};
 };
